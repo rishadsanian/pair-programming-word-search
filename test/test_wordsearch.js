64e2b8d2cf9d7fad9822e7e4c39c9b40c37a3b21
Rishad Alam <rishad.s.a@gmail.com>
2023-06-07 18:50:06 -0400

    check surroundings method

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,8 +3,8 @@ const assert = chai.assert;
 
 const wordSearch = require("../wordsearch.js");
 
-describe("#wordSearch()", function () {
-  it("should return false if the word is not present", function () {
+describe("#wordSearch()", function() {
+  it("should return false if the word is not present", function() {
     const result = wordSearch(
       [
         ["A", "W", "C", "F", "Q", "U", "A", "L"],
@@ -23,7 +23,7 @@ describe("#wordSearch()", function () {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function () {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch(
       [
         ["A", "W", "C", "F", "Q", "U", "A", "L"],
@@ -42,7 +42,7 @@ describe("#wordSearch()", function () {
     assert.isTrue(result);
   });
 
-  it("should return true if the word is present by searching backwards", function () {
+  it("should return true if the word is present by searching horizontally backwards", function() {
     const result = wordSearch(
       [
         ["A", "W", "C", "F", "Q", "U", "A", "L"],
@@ -61,7 +61,47 @@ describe("#wordSearch()", function () {
     assert.isTrue(result);
   });
 
-  it("should return true if the word is present by searching diagonally top left to bottom right", function () {
+  it("should return true if the word is present by searching vertically down", function() {
+    const result = wordSearch(
+      [
+        ["A", "W", "C", "F", "Q", "U", "V", "L"],
+        ["S", "E", "I", "N", "F", "E", "E", "D"],
+        ["Y", "F", "C", "F", "Q", "U", "R", "L"],
+        ["H", "M", "J", "T", "E", "V", "T", "G"],
+        ["W", "H", "C", "S", "Y", "E", "I", "L"],
+        ["B", "F", "R", "E", "N", "E", "C", "B"],
+        ["U", "B", "T", "W", "A", "P", "A", "I"],
+        ["O", "D", "C", "A", "K", "U", "L", "S"],
+        ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+      ],
+      "VERTICAL"
+    );
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present by searching vertically up", function() {
+    const result = wordSearch(
+      [
+        ["A", "W", "C", "F", "Q", "U", "A", "L"],
+        ["S", "L", "I", "N", "F", "E", "L", "D"],
+        ["Y", "A", "C", "F", "Q", "U", "A", "L"],
+        ["H", "C", "J", "T", "E", "V", "R", "G"],
+        ["W", "I", "C", "S", "Y", "E", "R", "L"],
+        ["B", "T", "R", "E", "N", "E", "Y", "B"],
+        ["U", "R", "T", "W", "A", "P", "A", "I"],
+        ["O", "E", "C", "A", "K", "U", "A", "S"],
+        ["E", "V", "K", "F", "Q", "U", "A", "L"],
+      ],
+      "PAW"
+    );
+
+    assert.isTrue(result);
+  });
+
+
+
+  it("should return true if the word is present by searching diagonally top left to bottom right", function() {
     const result = wordSearch(
       [
         ["A", "W", "C", "F", "Q", "U", "A", "L"],
@@ -80,7 +120,7 @@ describe("#wordSearch()", function () {
     assert.isTrue(result);
   });
 
-  it("should return true if the word is present by searching diagonally bottom left to top right", function () {
+  it("should return true if the word is present by searching diagonally bottom left to top right", function() {
     const result = wordSearch(
       [
         ["A", "W", "S", "F", "Q", "U", "A", "L"],
@@ -99,7 +139,7 @@ describe("#wordSearch()", function () {
     assert.isTrue(result);
   });
 
-  it("should return true if the word is present by searching diagonally bottom right to top left", function () {
+  it("should return true if the word is present by searching diagonally bottom right to top left", function() {
     const result = wordSearch(
       [
         ["A", "W", "S", "F", "Q", "U", "A", "L"],
@@ -118,7 +158,7 @@ describe("#wordSearch()", function () {
     assert.isTrue(result);
   });
 
-  it("should return true if the word is present by searching diagonally top right to bottom left", function () {
+  it("should return true if the word is present by searching diagonally top right to bottom left", function() {
     const result = wordSearch(
       [
         ["A", "W", "S", "F", "Q", "U", "A", "L"],
--- wordsearch.js
+++ wordsearch.js
@@ -1,6 +1,94 @@
 const wordSearch = (letters, word) => {
+
+  //Check Surroundings method
+  const rows = letters.length;
+  const cols = letters[0].length;
+  const totalLetters = rows * cols;
+  const search = [];//records positive search letter matches
+  let count = 0;//to keep track of how many letters it scanned through to get first letter match
+  let wordIndex = 0; //index of each letter in word
+  let i = 0;
+  let j = 0;
+  const directions = {
+    right: [0, 1],
+    left: [0, -1],
+    up: [-1, 0],
+    down: [1, 0],
+    diag1: [1, 1],
+    diag2: [-1, 1],
+    diag3: [1, -1],
+    diag4: [-1, -1],
+  };
+
+  const checkSurrounding = (searchLetter, wordLetter) => {
+    if (searchLetter !== wordLetter) {//if first letter doesn't match
+      count++;
+      return false;
+    }
+    
+    search.push(searchLetter);
+    wordIndex++;
+
+    while (search.length < word.length || count < totalLetters) {
+      for (const direction in directions) {//set direction
+        const [di, dj] = directions[direction];
+        const newI = i + di;
+        const newJ = j + dj;
+
+        if (newI >= 0 && newI < rows && newJ >= 0 && newJ < cols && letters[newI][newJ] === word[wordIndex]) {
+          i = newI;
+          j = newJ;
+          return true;
+        }
+      }
+      count++;
+      return false;
+    }
+
+    return search === word; // if fully matched
+  };
+
+  //
+  for (i = 0; i < rows; i++) {
+    for (j = 0; j < cols; j++) {//for each starting letter
+      if (checkSurrounding(letters[i][j], word[0])) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+
+
+/*
+
+  //Traverse method;
+  //horizontal
   const horizontalJoin = letters.map((ls) => ls.join(""));
   const backHorJoin = letters.map((ls) => ls.reverse().join(""));
+
+  //vertical
+
+  const transpose = function (matrix) {
+    // Replace this code with your solution
+    let output = [];
+    for (const index in matrix[0]) {
+      //determine the length of each array
+      let rows = [];
+      for (const array of matrix) {
+        //for all arrays convert  each column as row
+        rows.push(array[index]);
+      }
+      output.push(rows); //push rows to new array
+    }
+    return output;
+  };
+
+  const transposedGrid = transpose(letters);
+  const columnJoindown = transposedGrid.map((ls) => ls.join(""));
+  const columnJoinUp = transposedGrid.map((ls) => ls.reverse().join(""));
 
   // diagonals - does not work yet
   const rows = letters.length;
@@ -33,6 +121,7 @@ const wordSearch = (letters, word) => {
     }
   }
 
+  //
   for (let i = 1; i < rows; i++) {
     const diagonal = [];
     for (let j = 0; j < cols; j++) {
@@ -45,16 +134,38 @@ const wordSearch = (letters, word) => {
     }
   }
 
+  /////check this
+  // for (let i = 1; i < cols; i++) {
+  //   const diagonal = [];
+  //   for (let j = 0; j < rows; j++) {
+  //     if (letters[i] && letters[i + j]) {
+  //       diagonal.push(letters[j][i + j - 1]);
+  //     }
+  //   }
+  //   if (diagonal.length > 0) {
+  //     diagonals.push(diagonal);
+  //   }
+  // }
+
+  console.log(diagonals);
+
   const diagonalBackJoin = diagonals.map((ls) => ls.reverse().join(""));
   const digonalHorJoin = diagonals.map((ls) => ls.join(""));
 
-
-  //straight - works
+  //straight row - works
   for (const item of horizontalJoin) {
     if (item.includes(word)) return true;
   }
-  //bakwards - works
+  //bakwards row- works
   for (const item of backHorJoin) {
+    if (item.includes(word)) return true;
+  }
+
+  //column down
+  for (const item of columnJoindown) if (item.includes(word)) return true;
+
+  //column up
+  for (const item of columnJoindown) {
     if (item.includes(word)) return true;
   }
 
@@ -69,9 +180,9 @@ const wordSearch = (letters, word) => {
   }
 
   return false; //works
-};
+}; */
 
-console.log(
+/* console.log(
   wordSearch(
     [
       ["A", "W", "C", "F", "Q", "U", "A", "L"],
@@ -86,6 +197,6 @@ console.log(
     ],
     "WIFE"
   )
-);
+); */
 
 module.exports = wordSearch;
